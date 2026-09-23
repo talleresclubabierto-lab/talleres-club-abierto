a64f21eaf0f12ca804a46a932434af6b9b25d387
Club Abierto Talleres <talleresclubabierto@gmail.com>
2026-09-23 14:20:23 -0300

    Corregir import de PluginMethod en Capacitor 8

--- android/app/src/main/java/com/talleresclubabierto/app/NativeBiometricPlugin.java
+++ android/app/src/main/java/com/talleresclubabierto/app/NativeBiometricPlugin.java
@@ -15,7 +15,7 @@ import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
+import com.getcapacitor.PluginMethod;
 
 import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
